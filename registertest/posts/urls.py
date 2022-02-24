fe857354c206a208631b0bd8bddb06bc2c449fc4
dnlxo <wty8798@naver.com>
2022-02-25 01:58:13 +0900

    m

--- registertest/posts/urls.py
+++ registertest/posts/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     path('<int:post_id>', views.post_ru.as_view()),
 
     # /posts/3/comment POST요청 (3번 게시물에 댓 달기)
-    path('/<int:post_id>/comment', views.comment_cd.as_view()),
+    path('/<int:post_id>/comments', views.comment_cd.as_view()),
 
     # /posts/comment/12 DELETE요청 (12번 id를 가진 댓글 삭제)
-    path('/comment/<int:comment_id>', views.comment_cd.as_view()),
+    path('/comments/<int:comment_id>', views.comment_cd.as_view()),
 ]
